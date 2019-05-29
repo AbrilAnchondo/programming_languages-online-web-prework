458a506a484ff67ff3ca4da3721190fec10a5313
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-05-29 16:03:26 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |lang, properties|
+    if lang == :ruby
+      new_hash[lang] = {:style => [:oo]}
+     
+    end
+  end
+  new_hash
 end
